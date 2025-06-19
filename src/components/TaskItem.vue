aa275e63d0dc09da71989f35934e2de2c8652641
Leonid Petrov <lvpjsdev@gmail.com>
2025-06-19 18:00:43 +0300

    feat: tasks list

---
+++ src/components/TaskItem.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import { type Task } from '../types';
+interface Props {
+  task: Task;
+}
+
+const { task } = defineProps<Props>();
+
+const completed = reactive({
+  complete: task.completed,
+});
+</script>
+
+<template>
+  <p :class="completed">{{ task.title }}</p>
+</template>
+
+<style scoped>
+.complete {
+  text-decoration: line-through;
+  color: red;
+}
+</style>
---
+++ src/components/TasksList.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { taskApi } from '../api/api';
+import TaskItem from './TaskItem.vue';
+const tasks = await taskApi.getAll();
+</script>
+
+<template>
+  <TaskItem v-for="task in tasks" :task="task" :key="task.id" />
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
